f959a8a5bf9c5f863370a59bb03f4b1a1ffb8bc6
StivenVanegas <nevits28@hotmail.com>
2021-01-27 10:04:42 -0500

    Parte I inicial

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -4,9 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
+		//PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
 		
-		pft.start();
+		//pft.start();
+		
+		PrimeFinderThread pft1=new PrimeFinderThread(0, 9999999);
+		PrimeFinderThread pft2=new PrimeFinderThread(10000000, 19999999);
+		PrimeFinderThread pft3=new PrimeFinderThread(20000000, 30000000);
+		
+		pft1.start();
+		pft2.start();
+		pft3.start();
 		
 		
 	}
