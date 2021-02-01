a55906c178bce97220bc606e4c50a0b9d05f3e91
StivenVanegas <nevits28@hotmail.com>
2021-02-01 14:21:22 -0500

    Avance parte I, item 3

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,13 +1,28 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		//PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
+		PrimeFinderThread pft=new PrimeFinderThread(0, 10000000);
 		
-		//pft.start();
+		pft.start();
 		
+		try{
+			Thread.sleep(5*1000);
+			pft.suspenderhilo();
+			Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+			System.out.println("Presione ENTER");
+			String line = myObj.nextLine();  // Read user input
+			if(line.equals("")){
+				pft.renaudarhilo();
+			}
+		} catch (InterruptedException e) {
+            e.printStackTrace();
+        }	
+		/*
 		PrimeFinderThread pft1=new PrimeFinderThread(0, 9999999);
 		PrimeFinderThread pft2=new PrimeFinderThread(10000000, 19999999);
 		PrimeFinderThread pft3=new PrimeFinderThread(20000000, 30000000);
@@ -15,6 +30,7 @@ public class Main {
 		pft1.start();
 		pft2.start();
 		pft3.start();
+		*/
 		
 		
 	}
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -10,10 +10,15 @@ public class PrimeFinderThread extends Thread{
 	
 	private List<Integer> primes=new LinkedList<Integer>();
 	
+	private boolean suspender;
+	private boolean pausar;
+	
 	public PrimeFinderThread(int a, int b) {
 		super();
 		this.a = a;
 		this.b = b;
+		this.suspender = false;
+		this.pausar = false;
 	}
 
 	public void run(){
@@ -22,10 +27,34 @@ public class PrimeFinderThread extends Thread{
 				primes.add(i);
 				System.out.println(i);
 			}
+			synchronized (this) {
+				while (suspender) {
+					try{
+						this.wait();
+					} catch (Exception e){
+						e.printStackTrace();
+					}
+				}
+				if (pausar) break;
+			}
 		}
-		
-		
 	}
+	
+	public synchronized void pausarhilo(){
+        pausar=true;
+        //lo siguiente garantiza que un hilo suspendido puede detenerse.
+        suspender=false;
+        notify();
+    }
+	
+	public synchronized void suspenderhilo(){
+        suspender=true;
+    }
+	
+	public synchronized void renaudarhilo(){
+        suspender=false;
+        notify();
+    }
 	
 	boolean isPrime(int n) {
 	    if (n%2==0) return false;
@@ -39,8 +68,5 @@ public class PrimeFinderThread extends Thread{
 	public List<Integer> getPrimes() {
 		return primes;
 	}
-	
-	
-	
 	
 }
